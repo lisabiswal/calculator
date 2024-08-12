a3433a7350c0325f0bbb76b64641e9cc4f200916
MONALISA BISWAL <monalisaabiswall@gmail.com>
2024-08-12 22:39:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let num1=11
+let num2=6
+document.getElementById("firstnum").textContent=num1
+document.getElementById("secondnum").textContent=num2
+
+function add(){
+    res.textContent=num1+num2
+}
+function sub(){
+    res.textContent=num1-num2
+}
+function mul(){
+    res.textContent=num1*num2
+}
+function div(){
+    res.textContent=num1/num2
+}
+function mod(){
+    res.textContent=num1%num2
+}
